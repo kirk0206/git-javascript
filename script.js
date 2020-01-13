5fd4f541a7a4f6d4d4051a3c3f9d2ee57a07bf8c
Keira Kirkpatrick <kirk0206@algonquinlive.com>
2020-01-13 15:33:04 -0500

    Adds index.html and script.js initial content

---
+++ script.js
@@ -0,0 +1,13 @@
+let name = 'Keira'
+console.log(`Hello, my name is ${name}`)
+
+let number = 5
+let product = number * 2
+console.log(`The number ${number} multiplied by 2 is ${number * 2}.`)
+
+console.log(`The number multiplied by 5 is ${product * 5}.`)
+
+console.log(`The result divided by 5 is ${(product * 5) / number}`)
+
+let finalValue = (product * 5) / number
+console.log(`the result minus 7 is ${finalValue - 7}.`)
